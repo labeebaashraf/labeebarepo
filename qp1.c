67c6129b3848d71e1ac475ebd27e052cce49e65f
labeebaashraf <66689073+labeebaashraf@users.noreply.github.com>
2020-07-29 18:52:39 +0530

    Add files via upload

---
+++ qp1.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include<string.h
+#include<strlib.h>
+voif main()
+{  
+    char A[10],B[10],C[10],D[50];
+    int i,j,k,flag;
+    printf("enter A");
+    scanf(" [^\n]",A);
+    printf("enter B");
+    printf(" [^\n]",B);
+    printf("enter C");
+    printf(" [^\n]",C);
+    printf("enter D");
+    printf(" [^\n]",D)
+    for(i=0;D[i]!='\0';i++
+      {
+          for(flag=0;k=i;j=0;A[j]!='\0';k++;j++
+            {
+                if (A[k]=='\0'
+                 {
+                     flag=1;
+                     break;
+                 } 
+                else if (D[k]!=A[j]
+                {
+                    flag=1
+                    break
+                }
+                
+            }    
+                if(flag==0
+                 {
+                     break
+                 }
+
+    
+}
