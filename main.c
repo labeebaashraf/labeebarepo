5d3eba1859d6e06e33d891f8fdc2e0061b675b93
labeebaashraf <66689073+labeebaashraf@users.noreply.github.com>
2020-07-29 12:19:11 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    
+    sum=a+b;
+    printf("sum:%d",sum);
+    return(0);
+}
